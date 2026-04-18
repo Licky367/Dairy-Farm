8e38390b42b559ce2a3241b856e28b1e72ed97bf
Licky367 <licky367@gmail.com>
2026-04-19 00:54:43 +0300

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,38 +4,45 @@ exports.dashboard = (req, res) => {
     res.render("admin/dashboard");
 };
 
-// list products
 exports.products = async (req, res) => {
     const products = await Product.find();
     res.render("admin/products", { products });
 };
 
-// edit page
 exports.editProductPage = async (req, res) => {
     const product = await Product.findOne({ id: req.params.id });
 
     if (!product) return res.send("Product not found");
 
-    res.render("admin/adminProduct", { product });
+    res.render("admin/editProduct", { product });
 };
 
-// update product
 exports.updateProduct = async (req, res) => {
-    const { name, description, image, cost, category, depositPercentage } = req.body;
+    const {
+        name,
+        category,
+        cost,
+        description,
+        depositPercentage
+    } = req.body;
 
-    const depositAmount = (cost * depositPercentage) / 100;
+    const updateData = {
+        name,
+        category,
+        cost,
+        description,
+        depositPercentage,
+        depositAmount: (cost * depositPercentage) / 100
+    };
+
+    // if new image uploaded
+    if (req.file) {
+        updateData.image = "/uploads/" + req.file.filename;
+    }
 
     await Product.findOneAndUpdate(
         { id: req.params.id },
-        {
-            name,
-            description,
-            image,
-            cost,
-            category,
-            depositPercentage,
-            depositAmount
-        }
+        updateData
     );
 
     res.redirect("/admin/products");
