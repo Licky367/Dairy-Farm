a17e1095b90eeca65dc1ec04ff92cd3e0a2d7346
Licky367 <licky367@gmail.com>
2026-04-18 22:38:06 +0300

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,16 +1,42 @@
-const Order = require("../models/Order");
 const Product = require("../models/Product");
 
 exports.dashboard = (req, res) => {
     res.render("admin/dashboard");
 };
 
-exports.orders = async (req, res) => {
-    const orders = await Order.find();
-    res.render("admin/orders", { orders });
-};
-
+// list products
 exports.products = async (req, res) => {
     const products = await Product.find();
     res.render("admin/products", { products });
+};
+
+// edit page
+exports.editProductPage = async (req, res) => {
+    const product = await Product.findOne({ id: req.params.id });
+
+    if (!product) return res.send("Product not found");
+
+    res.render("admin/adminProduct", { product });
+};
+
+// update product
+exports.updateProduct = async (req, res) => {
+    const { name, description, image, cost, category, depositPercentage } = req.body;
+
+    const depositAmount = (cost * depositPercentage) / 100;
+
+    await Product.findOneAndUpdate(
+        { id: req.params.id },
+        {
+            name,
+            description,
+            image,
+            cost,
+            category,
+            depositPercentage,
+            depositAmount
+        }
+    );
+
+    res.redirect("/admin/products");
 };
