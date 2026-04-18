9775335519f71e8312a28769c3aab04b4fb93ac0
Licky367 <licky367@gmail.com>
2026-04-19 01:19:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,32 +2,68 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const path = require("path");
+require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
 const profileRoutes = require("./routes/profile");
 const clientRoutes = require("./routes/client");
+const adminRoutes = require("./routes/admin");
+const checkoutRoutes = require("./routes/checkout");
+const productRoutes = require("./routes/productDetails");
+const mpesaRoutes = require("./routes/mpesa");
 
 const app = express();
 
+/* MongoDB Connection */
 mongoose.connect("mongodb://127.0.0.1:27017/authDB")
-.then(()=>console.log("MongoDB Connected"));
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
+/* View Engine */
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended:true}));
+/* Middlewares */
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
-app.use(session({
-    secret:"secretkey",
-    resave:false,
-    saveUninitialized:false
-}));
+/* Session */
+app.use(
+    session({
+        secret: "secretkey",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24
+        }
+    })
+);
 
+/* Global Variables for Views */
+app.use((req, res, next) => {
+    res.locals.user = req.session.user || null;
+    res.locals.cart = req.session.cart || [];
+    next();
+});
+
+/* Routes */
 app.use("/", authRoutes);
 app.use("/", profileRoutes);
 app.use("/", clientRoutes);
+app.use("/", adminRoutes);
+app.use("/", checkoutRoutes);
+app.use("/", productRoutes);
+app.use("/", mpesaRoutes);
 
-app.listen(3000,()=>{
+/* 404 */
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+/* Server */
+app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
