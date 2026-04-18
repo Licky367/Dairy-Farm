63ad4594299dd0eab3be221d8d4584ac115c83b7
Licky367 <licky367@gmail.com>
2026-04-18 18:38:39 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const path = require("path");
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/authDB")
+.then(()=>console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.use(session({
+    secret:"secretkey",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use("/", authRoutes);
+
+app.listen(3000,()=>{
+    console.log("Server running on port 3000");
+});
