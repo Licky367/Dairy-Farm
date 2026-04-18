c7f0acd93bfafbab70790340ecb3c5d014a0656d
Licky367 <licky367@gmail.com>
2026-04-19 00:52:01 +0300

    Create upload.js

---
+++ middleware/upload.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/uploads/");
+    },
+
+    filename: (req, file, cb) => {
+        const unique = Date.now() + path.extname(file.originalname);
+        cb(null, unique);
+    }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
