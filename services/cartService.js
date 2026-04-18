7578c0ad16cda7fd1aaecf9a18867797437f29df
Licky367 <licky367@gmail.com>
2026-04-18 21:20:21 +0300

    Update cartService.js

--- services/cartService.js
+++ services/cartService.js
@@ -1,22 +1,31 @@
 exports.addToCart = (req, product) => {
-    if (!req.session.cart) {
-        req.session.cart = [];
-    }
+    if (!req.session.cart) req.session.cart = [];
 
-    const existing = req.session.cart.find(item => item.id === product.id);
+    const cart = req.session.cart;
+
+    const existing = cart.find(item => item.id === product.id);
 
     if (existing) {
         existing.quantity += 1;
     } else {
-        req.session.cart.push({
+        cart.push({
             id: product.id,
             name: product.name,
             cost: product.cost,
-            quantity: 1
+            quantity: 1,
+            image: product.image
         });
     }
+
+    req.session.cart = cart;
 };
 
-exports.getCart = (req) => {
-    return req.session.cart || [];
+exports.removeFromCart = (req, id) => {
+    req.session.cart = (req.session.cart || []).filter(item => item.id !== id);
+};
+
+exports.getTotal = (req) => {
+    return (req.session.cart || []).reduce((sum, item) => {
+        return sum + item.cost * item.quantity;
+    }, 0);
 };
