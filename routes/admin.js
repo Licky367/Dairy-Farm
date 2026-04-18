6b1b542f4b2a1bfeb2ff14157cdd67e17df1f373
Licky367 <licky367@gmail.com>
2026-04-19 00:52:48 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { isAdmin } = require("../middleware/auth");
 const adminController = require("../controllers/adminController");
+const upload = require("../middleware/upload");
+const { isAdmin } = require("../middleware/auth");
 
-// dashboard
 router.get("/admin", isAdmin, adminController.dashboard);
 
-// products list
 router.get("/admin/products", isAdmin, adminController.products);
 
-// edit product page
 router.get("/admin/products/edit/:id", isAdmin, adminController.editProductPage);
 
-// update product
-router.post("/admin/products/update/:id", isAdmin, adminController.updateProduct);
+router.post(
+    "/admin/products/update/:id",
+    isAdmin,
+    upload.single("image"),
+    adminController.updateProduct
+);
 
 module.exports = router;
