63ef55573b4b2208e8308d124f9bc98902f94d90
Licky367 <licky367@gmail.com>
2026-04-18 18:41:42 +0300

    Create controllers/authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const authService = require("../services/authService");
+
+exports.signupPage = (req,res)=>{
+    res.render("signup");
+};
+
+exports.signup = async(req,res)=>{
+    await authService.createUser(req.body);
+    res.redirect("/login");
+};
+
+exports.loginPage = (req,res)=>{
+    res.render("login");
+};
+
+exports.login = async(req,res)=>{
+    const user = await authService.loginUser(req.body);
+
+    if(!user) return res.send("Invalid Credentials");
+
+    req.session.user = user;
+    res.send("Login Successful");
+};
+
+exports.forgotPage = (req,res)=>{
+    res.render("forgot-password");
+};
+
+exports.forgotPassword = async(req,res)=>{
+    const user = await authService.findEmail(req.body.email);
+
+    if(!user) return res.send("Email not found");
+
+    res.redirect(`/reset-password/${user._id}`);
+};
+
+exports.resetPage = (req,res)=>{
+    res.render("reset-password",{id:req.params.id});
+};
+
+exports.resetPassword = async(req,res)=>{
+    await authService.resetPassword(req.params.id, req.body.password);
+    res.redirect("/login");
+};
+
+exports.logout = (req,res)=>{
+    req.session.destroy(()=>{
+        res.render("logout");
+    });
+};
