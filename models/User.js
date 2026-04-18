c9d39e860ef6c7177c9c50a13bd922a8f7f0a572
Licky367 <licky367@gmail.com>
2026-04-18 19:37:39 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+
     name: {
       type: String,
       required: true,
