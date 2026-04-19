6a4254077ad4c85ce96352042759996e1305950a
Licky367 <licky367@gmail.com>
2026-04-19 10:18:52 +0300

    Update checkout.js

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const checkoutController = require("../controllers/checkoutController");
 
-router.get("/checkout", checkoutController.checkoutPage);
-router.post("/checkout", checkoutController.processCheckout);
+const checkoutController =
+require("../controllers/checkoutController");
+
+// checkout page
+router.get(
+"/checkout",
+checkoutController.checkoutPage
+);
+
+// create order + payment
+router.post(
+"/checkout",
+checkoutController.processCheckout
+);
 
 module.exports = router;
