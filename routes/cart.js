92baaefef4e859521083b92063361baf4df5b4bc
Licky367 <licky367@gmail.com>
2026-04-21 13:22:32 +0300

    Create cart.js

---
+++ routes/cart.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const cartController = require("../controllers/cartController");
+
+router.get("/", cartController.viewCart);
+
+router.post("/add/:id", cartController.addToCart);
+
+router.post("/remove/:id", cartController.removeFromCart);
+
+router.post("/increase/:id", cartController.increaseQty);
+
+router.post("/decrease/:id", cartController.decreaseQty);
+
+router.post("/clear", cartController.clearCart);
+
+module.exports = router;
