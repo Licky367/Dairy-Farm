93e19a2a9a323a220db91b99eba1e6efbcb7bf89
Licky367 <licky367@gmail.com>
2026-04-21 11:13:28 +0300

    Update adminService.js

--- services/adminService.js
+++ services/adminService.js
@@ -1,7 +1,7 @@
 const Product = require("../models/Product");
 
 exports.getProducts = async () => {
-    return await Product.find();
+    return await Product.find().sort({ createdAt: -1 });
 };
 
 exports.getProduct = async (id) => {
@@ -9,27 +9,41 @@ exports.getProduct = async (id) => {
 };
 
 exports.createProduct = async (data, file) => {
+    const cost = Number(data.cost) || 0;
+    const depositPercentage =
+        Number(data.depositPercentage) || 0;
+    const itemsAvailable =
+        Number(data.itemsAvailable) || 0;
+
     await Product.create({
         id: data.id,
         name: data.name,
         category: data.category,
         image: file ? "/uploads/" + file.filename : "",
-        cost: data.cost,
-        depositPercentage: data.depositPercentage,
+        cost: cost,
+        depositPercentage: depositPercentage,
         depositAmount:
-            (data.cost * data.depositPercentage) / 100,
+            (cost * depositPercentage) / 100,
+        itemsAvailable: itemsAvailable,
         description: data.description
     });
 };
 
 exports.updateProduct = async (id, data, file) => {
+    const cost = Number(data.cost) || 0;
+    const depositPercentage =
+        Number(data.depositPercentage) || 0;
+    const itemsAvailable =
+        Number(data.itemsAvailable) || 0;
+
     const updateData = {
         name: data.name,
         category: data.category,
-        cost: data.cost,
-        depositPercentage: data.depositPercentage,
+        cost: cost,
+        depositPercentage: depositPercentage,
         depositAmount:
-            (data.cost * data.depositPercentage) / 100,
+            (cost * depositPercentage) / 100,
+        itemsAvailable: itemsAvailable,
         description: data.description
     };
 
@@ -38,7 +52,11 @@ exports.updateProduct = async (id, data, file) => {
             "/uploads/" + file.filename;
     }
 
-    await Product.findOneAndUpdate({ id }, updateData);
+    await Product.findOneAndUpdate(
+        { id },
+        updateData,
+        { new: true }
+    );
 };
 
 exports.deleteProduct = async (id) => {
