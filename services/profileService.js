b88ce3a83bf03212df2a8c7be3228d89cd007197
Licky367 <licky367@gmail.com>
2026-04-23 23:07:16 +0300

    Update profileService.js

--- services/profileService.js
+++ services/profileService.js
@@ -1,13 +1,54 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
+/**
+ * Get user profile
+ */
 exports.getUser = async (id) => {
     return await User.findById(id);
 };
 
-exports.updateUser = async (id, data) => {
-    return await User.findByIdAndUpdate(id, {
+
+/**
+ * Update user profile
+ * - name/email/phone
+ * - optional profile image
+ * - optional password change
+ */
+exports.updateUser = async (id, data, file = null) => {
+
+    const updateData = {
         name: data.name,
         email: data.email,
         phone: data.phone
-    });
+    };
+
+    // 📸 PROFILE IMAGE (from multer)
+    if (file) {
+        updateData.profileImage = file.filename;
+    }
+
+    // 🔐 PASSWORD CHANGE LOGIC
+    if (data.newPassword || data.confirmNewPassword || data.password) {
+
+        const user = await User.findById(id);
+
+        const match = await bcrypt.compare(data.password, user.password);
+
+        if (!match) {
+            throw new Error("Current password is incorrect");
+        }
+
+        if (data.newPassword !== data.confirmNewPassword) {
+            throw new Error("New passwords do not match");
+        }
+
+        if (data.newPassword.length < 6) {
+            throw new Error("Password must be at least 6 characters");
+        }
+
+        updateData.password = await bcrypt.hash(data.newPassword, 10);
+    }
+
+    return await User.findByIdAndUpdate(id, updateData, { new: true });
 };
