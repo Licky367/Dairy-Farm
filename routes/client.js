4843b3b77b61cd2475d44b8addd4fa6de1cd977f
Licky367 <licky367@gmail.com>
2026-04-25 15:32:37 +0300

    Update client.js

--- routes/client.js
+++ routes/client.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const clientController = require("../controllers/clientController");
 
+// ================= LIST PAGE =================
 router.get("/client", clientController.clientPage);
+
+// ================= PRODUCT VIEW =================
+router.get("/client/view/:id", clientController.viewProductPage);
 
 module.exports = router;
