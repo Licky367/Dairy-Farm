4d3bc5abd43bf259e5ace768480af759c195bdd2
Licky367 <licky367@gmail.com>
2026-04-27 19:13:28 +0300

    Create notificationsService.js

---
+++ services/notificationsService.js
@@ -0,0 +1,74 @@
+const User = require("../models/User");
+const Notification = require("../models/Notification");
+
+const {
+  sendEmailBulk,
+  sendSMSBulk,
+  sendWhatsAppBulk
+} = require("./providers"); // we’ll create this next
+
+exports.processNotification = async ({
+  channel,
+  message,
+  userIds,
+  sendToAll,
+  manualEmails = [],
+  manualPhones = [],
+  senderId
+}) => {
+  try {
+    let users = [];
+
+    if (sendToAll) {
+      users = await User.find();
+    } else if (userIds && userIds.length > 0) {
+      users = await User.find({ _id: { $in: userIds } });
+    }
+
+    const emails = users.map(u => u.email);
+    const phones = users.map(u => u.phone);
+
+    // merge manual inputs
+    const finalEmails = [...new Set([...emails, ...manualEmails])];
+    const finalPhones = [...new Set([...phones, ...manualPhones])];
+
+    let recipientsUsed = [];
+
+    if (channel === "email") {
+      await sendEmailBulk(finalEmails, message);
+      recipientsUsed = finalEmails;
+    }
+
+    if (channel === "sms") {
+      await sendSMSBulk(finalPhones, message);
+      recipientsUsed = finalPhones;
+    }
+
+    if (channel === "whatsapp") {
+      await sendWhatsAppBulk(finalPhones, message);
+      recipientsUsed = finalPhones;
+    }
+
+    await Notification.create({
+      channel,
+      message,
+      recipients: recipientsUsed,
+      status: "sent",
+      sentBy: senderId
+    });
+
+    return { success: true };
+
+  } catch (error) {
+    await Notification.create({
+      channel,
+      message,
+      recipients: [],
+      status: "failed",
+      error: error.message,
+      sentBy: senderId
+    });
+
+    throw error;
+  }
+};
