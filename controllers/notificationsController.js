a74546b736dd4607f592cf62274a3ad4665bd43b
Licky367 <licky367@gmail.com>
2026-04-27 19:12:23 +0300

    Create notificationsController.js

---
+++ controllers/notificationsController.js
@@ -0,0 +1,36 @@
+const notificationService = require("../services/notificationsService");
+
+exports.renderPage = async (req, res) => {
+  res.render("notifications", {
+    title: "Notifications"
+  });
+};
+
+exports.sendNotification = async (req, res) => {
+  try {
+    const {
+      channel,
+      message,
+      userIds,
+      sendToAll,
+      manualEmails,
+      manualPhones
+    } = req.body;
+
+    await notificationService.processNotification({
+      channel,
+      message,
+      userIds: userIds ? [].concat(userIds) : [],
+      sendToAll: sendToAll === "on",
+      manualEmails: manualEmails ? manualEmails.split(",") : [],
+      manualPhones: manualPhones ? manualPhones.split(",") : [],
+      senderId: req.user._id
+    });
+
+    res.redirect("/notifications?success=1");
+
+  } catch (error) {
+    console.error(error);
+    res.redirect("/notifications?error=1");
+  }
+};
